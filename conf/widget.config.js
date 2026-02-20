e4d79fcba5c7b061d93828b09583e30156b5005f
alllike996 <haohuoxing@gmail.com>
2026-02-20 10:54:34 +0800

    Update widget.config.js

--- conf/widget.config.js
+++ conf/widget.config.js
@@ -71,7 +71,7 @@ module.exports = {
     {
       name: '想你一次起一阵风',
       artist: '大潞',
-      url: 'https://sunhuo95.zabc.net/file/images/1756347900954_xnycdalu.m4a',// GitHub CDN加速：https://cdn.jsdelivr.net/gh/qinglong9627/NotionNext/public/music/xnycdalutg.m4a
+      url: 'https://bed.273983.xyz/file/images/1756347900954_xnycdalu.m4a',// GitHub CDN加速：https://cdn.jsdelivr.net/gh/qinglong9627/NotionNext/public/music/xnycdalutg.m4a
       cover:
         'https://p2.music.126.net/sfvZzeyK4OMxFnpVKEPX0A==/109951171850485702.jpg',
       lrc: '/lrc/想你一次起一阵风 - 大潞.lrc' 
@@ -79,7 +79,7 @@ module.exports = {
     {
       name: '凭什么你回头我就在',
       artist: '杨不乖',
-      url: 'https://sunhuo95.zabc.net/file/images/1762397449982_pingshmyangbuguai.mp3',// GitHub CDN加速：https://cdn.jsdelivr.net/gh/qinglong9627/NotionNext/public/music/pingshmyangbuguai.mp3
+      url: 'https://bed.273983.xyz/file/images/1762397449982_pingshmyangbuguai.mp3',// GitHub CDN加速：https://cdn.jsdelivr.net/gh/qinglong9627/NotionNext/public/music/pingshmyangbuguai.mp3
       cover:
         'https://p.sda1.dev/28/5c0cded086c2dea77eb1c0b993e77b44/tos-cn-avt-0015_60c6d78b43de387e9ec85583b23cb7e6~c5_300x300.jpeg',
       lrc: '/lrc/凭什么你回头我就在 - 杨不乖.lrc' 
