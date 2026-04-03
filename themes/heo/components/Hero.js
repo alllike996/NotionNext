b435612eae2f4372fa056849012153a736bc19a9
alllike996 <haohuoxing@gmail.com>
2026-04-03 22:30:57 +0800

    Update Hero.js

--- themes/heo/components/Hero.js
+++ themes/heo/components/Hero.js
@@ -89,7 +89,7 @@ function Banner(props) {
           <br />
           {siteConfig('HEO_HERO_TITLE_2', null, CONFIG)}
         </div>
-        <div className='text-4xl font-bold dark:text-white'>
+        <div className='text-xs text-gray-600  dark:text-gray-200'>
           {siteConfig('HEO_HERO_TITLE_3', null, CONFIG)}
         </div>
       </div>
